f1be7b38f45241ba02cb3a92e39457a446996446
nisha <nishantkumarsingh098@gmail.com>
2026-09-07 21:36:25 +0530

    Eureka related change

--- src/main/java/org/codesnippet/ecomorderservice/client/InventoryClient.java
+++ src/main/java/org/codesnippet/ecomorderservice/client/InventoryClient.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "inventory-service",
-        url = "http://localhost:8081",
+@FeignClient(name = "ecom-inventory-service",
         configuration = InventoryFeignClientConfig.class
 )
 public interface InventoryClient {
